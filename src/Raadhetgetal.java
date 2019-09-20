96104eb8c98ecb8d38519c933229d1e9112e0471
Esther <esther191299@gmail.com>
2019-09-20 17:18:10 +0200

    First commit

---
+++ src/Raadhetgetal.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Raadhetgetal {
+
+	public static void main(String[] args) {
+		Scanner textInput = new Scanner(System.in);
+		
+		System.out.print("raad het getal ");
+		int playerInput = textInput.nextInt();
+		
+		//case1
+		System.out.print("Het getal is hoger");
+		//case2
+		System.out.print("Het getal is lager");
+		
+		/*if "playerInput" > "randomNumber"{
+		 System.out.print("Het getal is lager");
+		 }
+		else if "playerInput" < "randomNumber"{
+		System.out.print("Het getal is hoger");
+		 }
+		else
+		System.out.print("Het getal Juist. U heeft " + numberOfTrys + ("keer geraden");
+		
+		*/
+	}
+
+}

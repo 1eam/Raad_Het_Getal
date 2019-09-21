25595381dd70a84e6894ad327c61c15d9a91c884
Esther <esther191299@gmail.com>
2019-09-21 17:08:52 +0200

    Added Rounds + updated Layout

--- src/RaadHetGetal.java
+++ src/RaadHetGetal.java
@@ -6,34 +6,37 @@ public class RaadHetGetal {
 		Scanner textInput = new Scanner(System.in);
 
 		boolean gameWon = false;
-		int randomNumber = (int)(Math.random()*1000);
-
+		int randomNumber = (int)(Math.random()*3);
+		int round = 2;
 
 		//TODO loopen als condition false blijft
 
-		if (gameWon == false) for (int i = 0; i >= 0; i++){
-			System.out.print("raad het getal ");
-			int playerInput = textInput.nextInt();
+		if (gameWon == false)
+			for (int i = 1; i >= 1; i++){
+				System.out.print("\nraad het getal ");
+				int playerInput = textInput.nextInt();
 
-			if (playerInput == randomNumber) {
-				gameWon = true;
-				randomNumber = (int)(Math.random()*1000);
+				if (playerInput == randomNumber) {
+					gameWon = true;
+					randomNumber = (int)(Math.random()*3);
+				}
+
+				else if (playerInput < randomNumber)
+					System.out.println("[Het getal is hoger]");
+
+
+				else if (playerInput > randomNumber)
+					System.out.println("[Het getal is lager]");
+
+
+				if (gameWon == true) {
+					System.out.println("\n\t [Men You guessed it! Whoo...]");
+					System.out.println("\t [It took you " + i + " try's]");
+					System.out.println("\n/// round " + round++ + " \\\\\\");
+					gameWon = false;
+				}
 			}
-
-			else if (playerInput < randomNumber)
-				System.out.println("Het getal is hoger");
-
-
-			else if (playerInput > randomNumber)
-				System.out.println("Het getal is lager");
-
-
-			if (gameWon == true) {
-				System.out.println("Men You guessed it! Whoo...");
-				System.out.println("It took you " + i + " try's");
-				gameWon = false;
-			}
-		}
+			
 
 		/*if "playerInput" > "randomNumber"{
 		 System.out.print("Het getal is lager");

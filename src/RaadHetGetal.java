8ddf0011356a8f8ec3177462ef79d665ff5ca441
Esther <esther191299@gmail.com>
2019-09-21 17:50:31 +0200

    Code cleanup + added number of trys

--- src/RaadHetGetal.java
+++ src/RaadHetGetal.java
@@ -3,51 +3,46 @@ import java.util.Scanner;
 public class RaadHetGetal {
 
 	public static void main(String[] args) {
+
 		Scanner textInput = new Scanner(System.in);
 
 		boolean gameWon = false;
 		int randomNumber = (int)(Math.random()*3);
 		int round = 2;
 
-		//TODO loopen als condition false blijft
+
 
 		if (gameWon == false)
-			for (int i = 1; i >= 1; i++){
+			for (int i = 0; i >= 0; i++){
+				int trys = 0 ;
+				
+				
 				System.out.print("\nraad het getal ");
 				int playerInput = textInput.nextInt();
 
-				if (playerInput == randomNumber) {
-					gameWon = true;
-					randomNumber = (int)(Math.random()*3);
+				if (playerInput < randomNumber) {
+					trys ++;
+					System.out.println("[Het getal is hoger]");
 				}
 
-				else if (playerInput < randomNumber)
-					System.out.println("[Het getal is hoger]");
-
-
-				else if (playerInput > randomNumber)
+				else if (playerInput > randomNumber) {
+					trys ++;
 					System.out.println("[Het getal is lager]");
+				}
 
+				else if (playerInput == randomNumber)
+					gameWon = true;
 
 				if (gameWon == true) {
-					System.out.println("\n\t [Men You guessed it! Whoo...]");
-					System.out.println("\t [It took you " + i + " try's]");
+					System.out.println("\n\t\t [Men You guessed it! Whoo...]");
+					System.out.println("\t\t [It took you " + trys + " try's]");
 					System.out.println("\n/// round " + round++ + " \\\\\\");
+					randomNumber = (int)(Math.random()*3);
 					gameWon = false;
 				}
 			}
-			
-
-		/*if "playerInput" > "randomNumber"{
-		 System.out.print("Het getal is lager");
-		 }
-		else if "playerInput" < "randomNumber"{
-		System.out.print("Het getal is hoger");
-		 }
-		else
-		System.out.print("Het getal Juist. U heeft " + numberOfTrys + ("keer geraden");
-
-		 */
 	}
-
 }
+//code cleanup
+//trying to add try's
+//try's stay at 0

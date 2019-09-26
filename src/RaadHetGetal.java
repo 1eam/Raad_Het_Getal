149c29ae1ef570c42a39a98d1ba4ac0eb777ed8c
Esther <esther191299@gmail.com>
2019-09-26 21:56:32 +0200

    Code cleanup from last commit

--- src/RaadHetGetal.java
+++ src/RaadHetGetal.java
@@ -13,44 +13,42 @@ public class RaadHetGetal {
 		int playerTries = 1;
 
 		for (int gameLoop = 0; gameLoop >= 0; gameLoop++) {
-		if (gameWon == false) {
-			for (int i = 0; i >= 0; i++) {
+			if (gameWon == false) {
+				for (int i = 0; i >= 0; i++) {
 
-				System.out.print("\nraad het getal ");
+					System.out.print("\nraad het getal ");
+					int playerInput = numberScanner.nextInt();
+
+					if (playerInput < randomNumber) {
+						playerTries++;
+						System.out.println("[Het getal is hoger]");
+					}
+					if (playerInput > randomNumber) {
+						playerTries++;
+						System.out.println("[Het getal is lager]");
+					}
+					if (playerInput == randomNumber) {
+						gameWon = true;
+						break;
+					}
+				}
+			}
+
+			if (gameWon == true) {
+				System.out.println("\n\t\t [  You guessed it!  ]");
+				System.out.println("\t\t [It took you " + playerTries + " tries]");
+				System.out.println("\n\t\t ~~Rematch 1 = Yes/ 2 = N0~~");
 				int playerInput = numberScanner.nextInt();
 
-				if (playerInput < randomNumber) {
-					playerTries++;
-					System.out.println("[Het getal is hoger]");
+				if (playerInput == 1) {
+					System.out.println("\n/// round " + round++ + " \\\\\\");
+					playerTries = 1;
+					randomNumber = (int) (Math.random() * 2);
+					gameWon = false;
+				} else if (playerInput == 2) {
+					System.out.println("TODO: back to Keuzemenu");
 				}
-				if (playerInput > randomNumber) {
-					playerTries++;
-					System.out.println("[Het getal is lager]");
-				}
-				if (playerInput == randomNumber) {
-					gameWon = true;
-					break;
-				}
-
 			}
-		}
-
-		if (gameWon == true) {
-			System.out.println("\n\t\t [  You guessed it!  ]");
-			System.out.println("\t\t [It took you " + playerTries + " tries]");
-			System.out.println("\n\t\t ~~Rematch 1 = Yes/ 2 = N0~~");
-			int playerInput = numberScanner.nextInt();
-
-			if (playerInput == 1) {
-				System.out.println("\n/// round " + round++ + " \\\\\\");
-				playerTries = 1;
-				randomNumber = (int) (Math.random() * 2);
-				gameWon = false;
-			}
-			else if (playerInput == 2) {
-				System.out.println("Done");
-			}
-		}
 		}
 	}
 }

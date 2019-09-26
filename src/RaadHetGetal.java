b4a31b836a9cc485b595e96f9c14fac3f734854d
Esther <esther191299@gmail.com>
2019-09-26 17:30:14 +0200

    Preparation for Minigames

--- src/RaadHetGetal.java
+++ src/RaadHetGetal.java
@@ -7,38 +7,43 @@ public class RaadHetGetal {
 		Scanner textInput = new Scanner(System.in);
 
 		boolean gameWon = false;
-		int randomNumber = (int)(Math.random()*3);
+		int randomNumber = (int) (Math.random() * 1);
 		int round = 2;
-
-
+		int playerTries = 1;
 
 		if (gameWon == false)
-			for (int i = 0; i >= 0; i++){
-				int trys = 0 ;
-				
-				
+			for (int i = 0; i >= 0; i++) {
+
 				System.out.print("\nraad het getal ");
 				int playerInput = textInput.nextInt();
 
 				if (playerInput < randomNumber) {
-					trys ++;
+					playerTries++;
 					System.out.println("[Het getal is hoger]");
 				}
 
 				else if (playerInput > randomNumber) {
-					trys ++;
+					playerTries++;
 					System.out.println("[Het getal is lager]");
 				}
 
 				else if (playerInput == randomNumber)
 					gameWon = true;
 
-				if (gameWon == true) {
-					System.out.println("\n\t\t [Men You guessed it! Whoo...]");
-					System.out.println("\t\t [It took you " + trys + " try's]");
-					System.out.println("\n/// round " + round++ + " \\\\\\");
-					randomNumber = (int)(Math.random()*3);
-					gameWon = false;
+				if (gameWon == true) {// Add Round, reset number of trys
+					System.out.println("\n\t\t [  You guessed it!  ]");
+					System.out.println("\t\t [It took you " + playerTries + " tries]");
+
+					System.out.println("\n\t\t ~~Enter 1 for rematch~~ \n\t\t ~~Enter 2 for exit~~");
+
+					if (playerInput == 1) {
+						System.out.println("\n/// round " + round++ + " \\\\\\");
+						playerTries = 1;
+						randomNumber = (int) (Math.random() * 1);
+						gameWon = false;
+					} else if (playerInput == 2) {
+						// print keuzemenu
+					}
 				}
 			}
 	}
